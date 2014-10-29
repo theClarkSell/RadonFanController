dc6c8c7ec79264f937fe538d6065f788b042ec52
Clark Sell <clark.sell@csell.net>
2014-10-29 14:15:37 +0000

    updated pins

--- cylonDefault.js
+++ cylonDefault.js
@@ -2,10 +2,10 @@ Cylon = require('cylon');
 
 Cylon.robot({
   connection: { name: 'raspi', adaptor: 'raspi' },
-  device: { name: 'led', driver: 'led', pin: 17 },
+  device: { name: 'led', driver: 'led', pin: 11 },
   work: function(my) {
-    every((1).second(), function() {
-      my.led.toggle();
+    return every(1..second(), function() {
+      return my.led.toggle();
     });
   }
 }).start();
