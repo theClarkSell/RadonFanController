641f517b37b67f801d1623746c330a37a431cf8b
Clark Sell <clark.sell@csell.net>
2014-11-26 07:42:09 -0600

    added the relay spike

--- cylonDefault.js
+++ cylonDefault.js
@@ -2,7 +2,7 @@ var Cylon = require('cylon');
 var sense = require('ds18b20');
 
 Cylon.robot({
-  connection: { name: 'raspi', adaptor: 'raspi' },
+  	connection: { name: 'raspi', adaptor: 'raspi' },
   /*
   device: { 
   	led: { driver: 'led', pin: 11, connection: 'raspi' },
@@ -10,22 +10,24 @@ Cylon.robot({
   },
 
 */
+  	device: [ 
+  		{ name: 'led', driver: 'led', pin: 11 }, 
+  		{ name: 'relay', driver: 'led', pin: 16 }  
+	],
 
-  device: [ { name: 'led', driver: 'led', pin: 11 }, { name: 'led2', driver: 'led', pin: 16 }  ],
+	work: function(my) {
+		return every(1..second(), function() {
+			sense.sensors(function(err, ids) {
+		  		sense.temperature(ids, function(err, value) {
+		  			var temp = value * 9 / 5 + 32;
 
-  work: function(my) {
-    return every(1..second(), function() {
-		sense.sensors(function(err, ids) {
-	  		sense.temperature(ids, function(err, value) {
-	  			var temp = value * 9 / 5 + 32;
-
-		  		console.log('Current temperature is', temp);
+			  		console.log('Current temperature is', temp);
+				});
 			});
+
+			my.relay.toggle();
+
+			return my.led.toggle();
 		});
-
-		//my.relay.toggle();
-
-  		return my.led.toggle();
-    });
-  }
+	}
 }).start();
