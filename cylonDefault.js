0e76d83f0568702787b7167ff9e1c70f80400b41
Clark Sell <clark.sell@csell.net>
2014-11-25 22:05:40 -0600

    added temp npm modules

--- cylonDefault.js
+++ cylonDefault.js
@@ -1,11 +1,21 @@
-Cylon = require('cylon');
+var Cylon = require('cylon');
+var sense = require('ds18b20');
 
 Cylon.robot({
   connection: { name: 'raspi', adaptor: 'raspi' },
   device: { name: 'led', driver: 'led', pin: 11 },
   work: function(my) {
     return every(1..second(), function() {
-      return my.led.toggle();
+		sense.sensors(function(err, ids) {
+		  console.log('sensor id', ids); // got sensor IDs ...
+		});
+
+		// ...
+		sense.temperature('28-00000652eb08', function(err, value) {
+		  console.log('Current temperature is', value);
+		});
+
+  		return my.led.toggle();
     });
   }
 }).start();
