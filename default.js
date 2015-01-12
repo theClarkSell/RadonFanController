b9aa2d66dd1e7bee38bc816c7204b1014e183aa3
Clark Sell <clark.sell@csell.net>
2015-01-12 13:58:17 -0600

    updating pin write

--- default.js
+++ default.js
@@ -32,7 +32,7 @@ gpio.setup(_pinFan, gpio.DIR_OUT, onSetup);
 
 //just a test...
 gpio.setup(11, gpio.DIR_OUT, onSetup);
-//gpio.write(7, true, done);
+gpio.write(11, true, done);
 
 var tempFunc = function () {
 	console.log(Date.now(), '>> checking temp');
