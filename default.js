374c56192702427778c394bb4adf1b30f2f37670
Clark Sell <clark.sell@csell.net>
2015-02-13 20:39:40 -0600

    fixing post bug

--- default.js
+++ default.js
@@ -85,34 +85,39 @@ function tempFunc () {
 
 function postToM2x(temp) {
 	
-	var post_data = {
-		value: temp
-	};
-	var post_length = JSON.stringify(post_data).length;
+	var lastTemp = temp;
 
-	var post_options = {
-		host: 'api-m2x.att.com',
-		port: '80',
-		path: '/v2/devices/6db7bd071d27c8baccb77c544a3ceeaa/streams/temp/value',
-		method: 'PUT',
-		headers: {
-		  'Content-Type': 'application/json',
-		  'X-M2X-KEY': '82a2c1052b94a2ea4522ceabc864492d',
-		  'Content-Length': post_length
-		}
-  	};
+	if (temp !== lastTemp) {
+		var post_data = {
+			value: temp
+		};
 
-	// Set up the request
-	var post_req = http.request(post_options, function(res) {
-		res.setEncoding('utf8');
-		res.on('data', function (chunk) {
-			console.log('M2X Response: ' + chunk);
+		var post_length = JSON.stringify(post_data).length;
+
+		var post_options = {
+			host: 'api-m2x.att.com',
+			port: '80',
+			path: '/v2/devices/6db7bd071d27c8baccb77c544a3ceeaa/streams/temp/value',
+			method: 'PUT',
+			headers: {
+			  'Content-Type': 'application/json',
+			  'X-M2X-KEY': '82a2c1052b94a2ea4522ceabc864492d',
+			  'Content-Length': post_length
+			}
+	  	};
+
+		// Set up the request
+		var post_req = http.request(post_options, function(res) {
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+				console.log('M2X Response: ' + chunk);
+			});
 		});
-	});
 
-	post_req.write(JSON.stringify(post_data));
-	post_req.end();
-
+		post_req.write(JSON.stringify(post_data));
+		post_req.end();
+	}
+	
 }
 
 function shouldFanBeRunning(temp, relay) {
